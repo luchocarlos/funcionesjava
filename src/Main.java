651084911d9f296745f6f6240aa5a1f018699a2d
lusi <islu@MacBook-Pro-de-LUIS.local>
2022-07-20 20:34:00 -0500

    funcion_class

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,27 @@ import java.awt.desktop.SystemSleepListener;
 public class Main {
     public static void main(String[] args) {
         sumas(8,6,7);
+        coche micoche = new coche();
+        micoche.sumarpuerta();
+        System.out.println("las puertas del coche son " + micoche.puertas);
+
 
     }
 
     public static int sumas(int param1, int param2, int param3){
         int suma;
         suma = param1 + param2 + param3;
-        System.out.println(suma);
+        System.out.println("el resultado de suma es " + suma);
         return suma;
+    }
+
+
+}
+
+class coche {
+    public int puertas= 4;
+
+    public void sumarpuerta(){
+        this.puertas++;
     }
 }
